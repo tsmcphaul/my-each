967799c4c7596b35c674ec3e4e181475c9876ed8
Tammarra McPhaul <shaemc22@gmail.com>
2021-01-17 23:09:33 -0500

    commit tree d08d54e07610920c97ce23ac57c8734cae7f1f5f

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,12 @@
-def my_each # put argument(s) here
+def my_each(array) # put argument(s) here
   # code here
+  if block_given?
+    i = 0
+
+    while i < array.length
+      yield(array[i])
+      i += 1
+    end
+    return array
+  end
 end
